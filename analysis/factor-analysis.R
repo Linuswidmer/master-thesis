5c64fd23ca5e5b4bc5cc366f095b0d8bb9088b1f
Linus Widmer <lwidmer.hb@gmail.com>
2025-11-11 20:54:21 +0100

    add helper functio to conduct cfa

--- analysis/factor-analysis.R
+++ analysis/factor-analysis.R
@@ -16,3 +16,50 @@ summarizeFitIndices <- function(model, indices = c("cfi", "tli", "rmsea", "srmr"
 
   return(fit_summary)
 }
+
+run_cfa_model <- function(model_syntax, data, model_name = "CFA_Model", show_mi = TRUE) {
+  message("\n===========================================================")
+  message("Running CFA: ", model_name)
+  message("===========================================================\n")
+
+  fit <- cfa(model_syntax, data = data, std.lv = TRUE)
+
+  # cat("\n-------------------------\n")
+  # cat("MODEL SUMMARY\n")
+  # cat("-------------------------\n")
+  # print(summary(fit, fit.measures = TRUE, standardized = TRUE))
+
+  cat("\n-------------------------\n")
+  cat("MODEL FIT INDICES\n")
+  cat("-------------------------\n")
+  fit_indices <- fitMeasures(
+    fit,
+    c("cfi", "tli", "rmsea", "srmr", "chisq", "df")
+  )
+  print(summarizeFitIndices(fit))
+
+  cat("\n-------------------------\n")
+  cat("STANDARDIZED FACTOR LOADINGS\n")
+  cat("-------------------------\n")
+  loadings <- standardizedSolution(fit) %>%
+    filter(op == "=~") %>%
+    select(Factor = lhs, Item = rhs, Std_Loading = est.std)
+  print(loadings)
+
+  if (show_mi) {
+    cat("\n-------------------------\n")
+    cat("TOP MODIFICATION INDICES\n")
+    cat("-------------------------\n")
+    mi <- modindices(fit, sort = TRUE, maximum.number = 5)
+    print(mi)
+  } else {
+    mi <- NULL
+  }
+
+  invisible(list(
+    fit = fit,
+    fit_indices = fit_indices,
+    loadings = loadings,
+    mod_indices = mi
+  ))
+}
