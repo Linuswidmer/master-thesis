986695767c3dcc6c19b8324f84dc67270a2125c2
Linuswidmer <lwidmer.hb@gmail.com>
2025-12-04 17:01:15 +0100

    add script to download packages

---
+++ analysis/R/install_packages.R
@@ -0,0 +1,37 @@
+# Install required packages for analysis-report.Rmd
+
+install.packages(c(
+  # Data manipulation
+  "dplyr",
+  "tidyr",
+  "stringr",
+
+  # Visualization
+  "ggplot2",
+  "ggpubr",
+  "corrplot",
+  "gridExtra",
+
+  # Reporting
+  "knitr",
+  "kableExtra",
+  "rmarkdown",
+
+  # Statistical analysis
+  "psych",
+  "car",
+  "boot",
+
+  # Structural equation modeling
+  "lavaan",
+  "semTools",
+
+  # Item response theory
+  "mirt",
+
+  # Utilities
+  "here",
+  "R.utils",
+  "rlang",
+  "jtools"
+))
